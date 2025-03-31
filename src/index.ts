cddefd87a97abdaf27bcf397a0ea6b119577e3d9
nobuoJT <mario.oga0512@gmail.com>
2025-03-31 13:15:03 +0900

    add feat [auto_diff_flag]

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,18 @@ import { readFile } from "fs/promises";
 import { Window } from "node-screenshots";
 import { GlobalKeyboardListener } from 'node-global-key-listener';
 import looksSame from 'looks-same';
-import axios from "axios";
 
-const version="1.0.0-Alpha4"
+const version="1.0.0-Alpha5"
 
 let prevImage=new Map();
 const URL="https://discord.com/api/webhooks/1356111408231747745/w7jY4QqkdUNHprEdaHen_-aC_xg5XkJzSrVdfRxe3TP3DoPmbiu0eOIzjax37qssHoSC"
 
 let windows = Window.all();
 const keyboard = new GlobalKeyboardListener();
+let auto_diff_flag=false
 
 console.log(`microShot v${version}`)
-console.log("'L' key to print window List.\n'R Ctrl' to Capture.\n'Esc' to exit.")
+console.log("'L' key to print window List.\n'R Ctrl' to Capture.\n'F10' to start auto diff notice. 'F9' to stop.\n'Esc' to exit.")
 //説明
 
 
@@ -66,33 +66,43 @@ keyboard.addListener((event:any) => {//キーボードイベント割り込み(�
     }
 
     if (event.name === 'F10' && event.state === 'DOWN') {//F10
-
-        readFileSync(".secret_targetWindows",{encoding:"utf-8"}).split("\r\n").forEach((tg_window,i,a)=>{
-            windows.forEach(async (item:Window,i) => {
-                
-                if(item.appName==tg_window){
-                    let image=item.captureImageSync()
-                    //let filename = `pix/${item.appName}_${date.toLocaleString().replace(/\//g,"_").replace(/:/g,"_")}.png`
-                    let result
-                    if(prevImage.get(i)!==undefined){
-                        result = await looksSame(prevImage.get(i),image.toPngSync()),{tolerance:5}
-                        //console.log(result?.equal)
-                        if(!result?.equal){
-                            try{
-                                const formData = new FormData()
-                                formData.append('file', new Blob([image.toPngSync()], { type: 'image/png' }), 'file.png')
-                                const response = await fetch(URL, {
-                                    method: 'POST',
-                                    body: formData
-                                });
-
-                            } catch (error) {console.error(error)}
-                        }
-                    }
-                    prevImage.set(i,image.toPngSync())
-                    
-                }
-            });
-        })
+        auto_diff_flag=true
+        console.log("auto_diff_flag=true")
+    }
+    if (event.name === 'F9' && event.state === 'DOWN') {//F9
+        auto_diff_flag=false
+        console.log("auto_diff_flag=false")
     }
 });
+
+
+setInterval(() => {
+    if(!auto_diff_flag){return}
+    
+    readFileSync(".secret_targetAutoWindows",{encoding:"utf-8"}).split("\r\n").forEach((tg_window,i,a)=>{
+        windows.forEach(async (item:Window,i) => {
+            
+            if(item.appName==tg_window){
+                let image=item.captureImageSync()
+                //let filename = `pix/${item.appName}_${date.toLocaleString().replace(/\//g,"_").replace(/:/g,"_")}.png`
+                let result
+                if(prevImage.get(i)!==undefined){
+                    result = await looksSame(prevImage.get(i),image.toPngSync()),{tolerance:5}
+                    //console.log(result?.equal)
+                    if(!result?.equal){
+                        try{
+                            const formData = new FormData()
+                            formData.append('file', new Blob([image.toPngSync()], { type: 'image/png' }), 'file.png')
+                            const response = await fetch(URL, {
+                                method: 'POST',
+                                body: formData
+                            });
+                        } catch (error) {console.error(error)}
+                    }
+                }
+                prevImage.set(i,image.toPngSync())
+                
+            }
+        });
+    })
+}, 5000);
